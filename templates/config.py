bbde81e58daf6e52c2f7abf9db453e4878e577e1
Helena Rasche <hxr@hx42.org>
2020-02-13 10:32:47 +0100

    support both db

--- templates/config.py
+++ templates/config.py
@@ -7,10 +7,21 @@ TIAAS_DOMAIN = '{{ tiaas_info.domain }}'
 DEBUG = False
 
 DATABASES = {
+    {% if tiaas_tiaas_use_sqlite %}
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '{{ tiaas_dir }}/db.sqlite3',
     },
+    {% else %}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ tiaas_tiaas_db_name }}',
+        'USER': '{{ tiaas_tiaas_db_user }}',
+        'PASSWORD': '{{ tiaas_tiaas_db_pass }}',
+        'HOST': '{{ tiaas_tiaas_db_host }}',
+        'PORT': '{{ tiaas_tiaas_db_port }}',
+    }
+    {% endif %}
     'galaxy': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '{{ tiaas_galaxy_db_name }}',
