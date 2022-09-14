fa261695d2f3c793858364e0c1e50c1c200eb7c1
Helena Rasche <hxr@hx42.org>
2022-09-14 11:14:48 +0200

    Add statsd support

--- templates/run.sh
+++ templates/run.sh
@@ -2,4 +2,4 @@
 . {{ tiaas_venv_dir }}/bin/activate
 cd {{ tiaas_dir }}/code/
 export PYTHONPATH={{ tiaas_dir }}:$PYTHONPATH
-exec gunicorn --bind {{ tiaas_listen_url | default("127.0.0.1:5000") }} tiaas.wsgi
+exec gunicorn --bind {{ tiaas_listen_url | default("127.0.0.1:5000") }} tiaas.wsgi {% if tiaas_statsd_host %}--statsd-host {{ tiaas_statsd_host }} --statsd-prefix {{ tiaas_statsd_prefix }}{% endif %}
